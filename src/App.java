e80433427ab21d189dbc441c269deb700b12481a
Your Namebangnguyen02 <bangyn3010@gmail>
2022-01-17 16:35:01 +0700

    bai2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in)
+        String strHoTen = "bang nguyen";
+        Sysmtem.out.println("bang nguyen: ");
+        strHoTen = sc.nextLine()
+        System.out.println("xin chao" + strHoTen);
     }
 }
